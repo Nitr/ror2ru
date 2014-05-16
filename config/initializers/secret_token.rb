f0b48e6b059486d2e200e91910f08ab7caad625b
Nitr <antynitr@gmail.com>
2014-05-16 15:08:43 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ror2ru::Application.config.secret_key_base = '3be2dfa0f34f6e6c1ae74d2623cfcf30ba2456251f57cd3b4dfa4175049a60add706b7c3033b09640f2dfe06113de0d83308f12e662d269d08a98a3f285e8b8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ror2ru::Application.config.session_store :cookie_store, key: '_ror2ru_session'
